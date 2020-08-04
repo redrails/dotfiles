4f886c21cd016b2fe823adcc7b783a4297ebb064
Ihtasham <redrailz@gmail.com>
2020-08-04 15:07:53 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,17 +1,17 @@
-alias reloadfish="source ~/.config/fish/config.fish"
-alias editfish="vim ~/.config/fish/config.fish"
+ alias reloadfish="source ~/.config/fish/config.fish"
+ alias editfish="vim ~/.config/fish/config.fish"
 
-alias tf="terraform"
+ alias tf="terraform"
 
-alias typora="open -a typora"
+ alias typora="open -a typora"
 
-alias gp=/Users/lonic9/gpmanager.sh
+ alias gp=/Users/lonic9/gpmanager.sh
 
-alias awsp="aws-profile-utils"
+ alias awsp="aws-profile-utils"
 
-set -x YVM_DIR /usr/local/opt/yvm
-. $YVM_DIR/yvm.fish
+ set -x YVM_DIR /usr/local/opt/yvm
+ . $YVM_DIR/yvm.fish
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+ # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-set PATH $HOME/.jenv/bin $PATH
+ set PATH $HOME/.jenv/bin $PATH
